a5ae0b24759bca54e62f4c286f5fb19d44d445f1
jardenna <hbjornum@gmail.com>
2021-04-16 09:29:28 +0200

    Added comment

--- src/js/components/managersDropdown/ManagerDropdown.js
+++ src/js/components/managersDropdown/ManagerDropdown.js
@@ -18,8 +18,14 @@ const Index = () => {
    const managers = result ? result : [];
 
    const newManagerArray = managers.data && managers.data.map(manager => {
-
+      //In order to find a conneced Id between managers.data and  manager.included
+      //connected Id example: managers.data.id= '12', manager.included.id= '13'
+      //The Id both for managers data and  manager included is a string
+      //To find the manager email we need to:
+      //add 1 to the selected managers.data.id and find connected Id in manager.included
+      //Convert manager id to a number and add 1
       const searchId = Number(manager.id) + 1;
+      //Convert number back to a string
       const searchIdString = searchId.toString();
       const emailAttributes = managers.included && managers.included.find(include => include.id === searchIdString).attributes;
 
